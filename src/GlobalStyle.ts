d9827d268d1b5f3b5f8557ce26f10306b1f5ca71
Mike P. Sinn <m@quantimodo.com>
2022-09-10 15:38:41 -0500

    Removed fonts

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
-import avertaFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-normal.woff2'
-import avertaBoldFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-bold.woff2'
+// import avertaFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-normal.woff2'
+// import avertaBoldFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-bold.woff2'
 
 const GlobalStyle = createGlobalStyle`
     html {
@@ -22,13 +22,13 @@ const GlobalStyle = createGlobalStyle`
     .MuiInputBase-root {
         width: 100% !important;
     }
-
-    @font-face {
-        font-family: 'Averta';
-        src: local('Averta'), local('Averta Bold'),
-        url(${avertaFont}) format('woff2'),
-        url(${avertaBoldFont}) format('woff');
-    }
 `
+//     @font-face {
+//         font-family: 'Averta';
+//         src: local('Averta'), local('Averta Bold'),
+//         url(${avertaFont}) format('woff2'),
+//         url(${avertaBoldFont}) format('woff');
+//     }
+// `
 
 export default GlobalStyle
